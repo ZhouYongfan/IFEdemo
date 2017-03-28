4cce8a69fd9e65799f16c90a403fe1b4d6822b79
ZhouYongfan <bupt2012210978@sina.com>
2017-03-28 23:06:24 +0800

    Create Test2_5.js

---
+++ Test2_5/Test2_5.js
@@ -0,0 +1,53 @@
+function createNode(num) {
+	var newItem = document.createElement("span");
+    newItem.style.height = num + 'px';
+	newItem.setAttribute("class","box");
+    return newItem;
+}
+
+document.getElementById("leftIn").onclick = function() {
+    var value = document.getElementById('input').value;
+    var queue = document.getElementById('queue');
+    if(queue.childNodes.length >= 60) {
+        alert('扎心了，老铁');
+    }
+    else if(value >= 10 && value <= 100) {
+        var newNode = createNode(value);
+        queue.insertBefore(newNode, queue.childNodes[0]);
+    }
+}
+
+document.getElementById("rightIn").onclick = function() {
+    var value = document.getElementById('input').value;
+    if(queue.childNodes.length >= 60) {
+        alert('扎心了，老铁');
+    }
+    else if(value >= 10 && value <= 100) {
+        var newNode = createNode(value);
+        document.getElementById("queue").appendChild(newNode);
+    }
+    
+}
+
+document.getElementById("leftOut").onclick = function() {
+    var queue = document.getElementById('queue');
+    queue.removeChild(queue.childNodes[0]);
+}
+
+document.getElementById("rightOut").onclick = function() {
+    var queue = document.getElementById('queue');
+    queue.removeChild(queue.lastChild);
+}
+
+document.getElementById("sort").onclick = function() {
+    var queue = document.getElementById('queue');
+    var len = queue.childNodes.length;
+    for(let j = 0; j < len - 1; j ++){
+        for(let i = 0; i < len - 1 - j; i ++) {
+            if(parseInt(queue.childNodes[i].style.height) > parseInt(queue.childNodes[i+1].style.height)){
+                var node = queue.removeChild(queue.childNodes[i]);
+                queue.insertBefore(node,queue.childNodes[i+1]);
+            }
+        }
+    }
+}
